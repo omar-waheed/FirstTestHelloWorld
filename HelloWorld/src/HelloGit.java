deb4cf16eb0df1e4e2336844519a69ef556057f1
omar-waheed <omar-waheed1@hotmail.com>
2013-11-15 19:13:29 -0800

    Made changes in printNumber method's return type (void --> String) and fixed the error (ints were out of range)

--- HelloWorld/src/HelloGit.java
+++ HelloWorld/src/HelloGit.java
@@ -1,19 +1,19 @@
 /**
  * @author Omar
  */
-public class HelloGit {
+public class helloGit {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
-		System.out.println(printNumbers(6693211123456432211));
+		System.out.println(printNumbers(669321112));
 
 	}
 	
-	public static void printNumbers(int nums){
+	public static String printNumbers(int nums){
 		String str = "";
 		str += nums;
-	}
-		System.out.println(str);
+	
+		return (str);
 	}
 
 }
