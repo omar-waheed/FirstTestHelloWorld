671f31aedddaeafd62495883b37e0324ae6d4233
omar-waheed <omar-waheed1@hotmail.com>
2013-11-15 19:01:06 -0800

    added a printNumber method. It takes an integer argument and prints it as a string.

--- HelloWorld/src/HelloGit.java
+++ HelloWorld/src/HelloGit.java
@@ -5,7 +5,15 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git!");
+		System.out.println(printNumbers(6693211123456432211));
 
+	}
+	
+	public static void printNumbers(int nums){
+		String str = "";
+		str += nums;
+	}
+		System.out.println(str);
 	}
 
 }
